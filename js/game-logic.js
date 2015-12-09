0b3843ed7924f760e2265cfd7fd3478b58f93c65
unknown <jeffreyhshapiro@gmail.com>
2015-12-08 21:35:03 -0500

    created successful logic for RPS

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,10 +1,34 @@
 $(document).ready(function(){
 
+  //Generate a random choice for computer
   var choices = ["rock", "paper", "scissor"]
 
-  //Computer generates a number corresponding to rock, paper, or scissors
+  //Get the value of users button click
 
-  var computerChoice = Math.floor(Math.random() * choices.length)
-  
+  $(".btn-block").on("click", function(){
+    var userChoice = $(this).attr("data-choice");
+    console.log(userChoice);
+    var computerChoice = Math.floor(Math.random() * choices.length)
+    console.log(choices[computerChoice]);
+
+    if (userChoice === "rock" && choices[computerChoice]  === "paper") {
+      alert("Computer wins")
+    } else if (userChoice === "rock" && choices[computerChoice]  === "scissor") {
+      alert("You win")
+    } else if (userChoice === "paper" && choices[computerChoice]  === "rock") {
+      alert("You win")
+    } else if (userChoice === "paper" && choices[computerChoice]  === "scissor") {
+      alert("Computer win")
+    } else if (userChoice === "scissor" && choices[computerChoice]  === "rock") {
+      alert("Computer win")
+    } else if (userChoice === "scissor" && choices[computerChoice]  === "paper") {
+      alert("You win")
+    } else {
+      alert(userChoice)
+      alert(choices[computerChoice])
+    }
+
+  });
+
 
 });
