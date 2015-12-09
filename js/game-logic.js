0da3f23c5bfcf8170b9ac9ca368682f7e3bff44e
unknown <jeffreyhshapiro@gmail.com>
2015-12-08 21:08:31 -0500

    created html skeleton and random number generator for computer choice generation

---
+++ js/game-logic.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  var choices = ["rock", "paper", "scissor"]
+
+  //Computer generates a number corresponding to rock, paper, or scissors
+
+  var computerChoice = Math.floor(Math.random() * choices.length)
+  
+
+});
