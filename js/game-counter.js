673cc3b5db2010a9b5c3256e2680bda813c67ec2
unknown <jeffreyhshapiro@gmail.com>
2015-12-10 19:24:12 -0500

    created functional score tracker

---
+++ js/game-counter.js
@@ -0,0 +1,8 @@
+window.RPS = {
+  gameState: {
+    userScore: 0,
+    computerScore: 0,
+    ties: 0,
+    roundCount: 1
+  }
+}
--- js/game-logic.js
+++ js/game-logic.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+  //Set counter to beginning variables
+
+  /*var gameState = {
+    "userScore":0, 
+    "computerScore":0, 
+    "ties":0, 
+    "roundCount":1
+  }*/
+
   //Generate a random choice for computer
   var choices = ["rock", "paper", "scissor"]
 
@@ -10,24 +19,63 @@ $(document).ready(function(){
     console.log(userChoice);
     var computerChoice = Math.floor(Math.random() * choices.length)
     console.log(choices[computerChoice]);
+    var tagName = $(this).data("type");
+
+    //Logic behind wins
 
     if (userChoice === "rock" && choices[computerChoice]  === "paper") {
       alert("Computer wins")
+      //computerScore++
+      RPS.gameState.computerScore++;
+      console.log(RPS.gameState.computerScore);
+      $("#computerScore").html(RPS.gameState.computerScore);
+
     } else if (userChoice === "rock" && choices[computerChoice]  === "scissor") {
       alert("You win")
+      //userScore++
+      RPS.gameState.userScore++;
+      console.log(RPS.gameState.userScore);
+      $("#userScore").html(RPS.gameState.userScore);
+
     } else if (userChoice === "paper" && choices[computerChoice]  === "rock") {
       alert("You win")
+      //userScore++
+      RPS.gameState.userScore++
+      console.log(RPS.gameState.userScore);
+      $("#userScore").html(RPS.gameState.userScore);
+
     } else if (userChoice === "paper" && choices[computerChoice]  === "scissor") {
       alert("Computer win")
+      //computerScore++
+      RPS.gameState.computerScore++;
+      console.log(RPS.gameState.computerScore);
+      $("#computerScore").html(RPS.gameState.computerScore);
+
     } else if (userChoice === "scissor" && choices[computerChoice]  === "rock") {
       alert("Computer win")
+      //computerScore++
+      RPS.gameState.computerScore++
+      console.log(RPS.gameState.computerScore);
+      $("#computerScore").html(RPS.gameState.computerScore);
+
     } else if (userChoice === "scissor" && choices[computerChoice]  === "paper") {
       alert("You win")
-    } else {
-      alert(userChoice)
-      alert(choices[computerChoice])
-    }
+      //userScore++
+      RPS.gameState.userScore++
+      console.log(RPS.gameState.userScore);
+      $("#userScore").html(RPS.gameState.userScore);
 
+    } else {
+      //Tie
+      alert("Tie!")
+      RPS.gameState.ties++;
+      console.log(ties);
+      $("#ties").html(RPS.gameState.ties);
+      
+    }
+    RPS.gameState.roundCount++;
+    $("#roundNumber").html(RPS.gameState.roundCount);
+    console.log(RPS.gameState.roundCount);
   });
 
 
