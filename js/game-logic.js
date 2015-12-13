417d78139c69e88a015cadbc15bc890caadb2681
unknown <jeffreyhshapiro@gmail.com>
2015-12-12 22:14:02 -0500

    commiting so i can merge and get rid of this monstrosity of code

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
 
   //Generate a random choice for computer
-  var choices = ["rock", "paper", "scissors"]
+  var choices = ["rock", "paper", "scissors"];
+
 
   //Get the value of users button click
 
-  $(".btn-primary").on("click", function(){
-    var userChoice = $(this).attr("data-choice");
-    console.log(userChoice);
-    var computerChoice = Math.floor(Math.random() * choices.length)
-    console.log(choices[computerChoice]);
-
+    $(".btn-primary").on("click", function(){
+      var userChoice = $(this).attr("data-choice");   
+      console.log(userChoice);   
+      var computerChoice = Math.floor(Math.random() * choices.length);
+      console.log(choices[computerChoice]);
+    });
+    
     //Logic behind wins
 
     if (userChoice === "rock" && choices[computerChoice]  === "paper") {
       rockPaper();
-      //alert("Computer wins")
       RPS.gameState.computerScore++;
       console.log(RPS.gameState.computerScore);
       $("#computerScore").html(RPS.gameState.computerScore);
@@ -87,37 +88,39 @@ $(document).ready(function(){
       $("#outcome").html("Game over!")
         if (RPS.gameState.userScore > RPS.gameState.computerScore) {
           $("#outcome").append("<div>You won! Much honor!</div>")
+          $(".btn-primary").unbind();
         } else if (RPS.gameState.ties > RPS.gameState.computerScore && RPS.gameState.userScore) {
           $("#outcome").append("<div>No winners here.</div>")
+          $(".btn-primary").unbind();
         } else if (RPS.gameState.userScore < RPS.gameState.computerScore) {
           $("#outcome").append("<div>You lost. Great dishonor.</div>")
+          $(".btn-primary").unbind();
         } 
-
-    $(".btn-primary").unbind();
     $("#reset-button").addClass("btn btn-danger btn-block").append("Play again!");
     $("#reset-button").on("click", function(){
-
-    RPS.gameState.userScore = 0
-    RPS.gameState.computerScore = 0
-    RPS.gameState.ties = 0
-    RPS.gameState.roundCount = 1
-
-    $("#userScore").html(RPS.gameState.userScore);
-    $("#computerScore").html(RPS.gameState.computerScore);
-    $("#ties").html(RPS.gameState.ties);
-    $("#roundNumber").html(RPS.gameState.roundCount);
-
-    $(".btn-primary").bind();
-    $("#outcome").remove();
-    $("#reset-button").remove();
-
+      rebind();
+      $(".btn-primary").bind();
     });
 
+    function rebind(){
+      $("#outcome").empty();
+      $("#reset-button").remove();
+      lizardLizard(); 
+      RPS.gameState.userScore = 0
+      RPS.gameState.computerScore = 0
+      RPS.gameState.ties = 0
+      RPS.gameState.roundCount = 1
+      $("#userScore").html(RPS.gameState.userScore);
+      $("#computerScore").html(RPS.gameState.computerScore);
+      $("#ties").html(RPS.gameState.ties);
+      $("#roundNumber").html(RPS.gameState.roundCount);
+    
 
-
+    };
     
     };
 
+
   });
 
-});
+//});
